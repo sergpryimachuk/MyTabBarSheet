e436a6462c51241d0feb085c3c14d11730085fa6
sergpryimachuk <serg@pryimach.uk>
2023-11-10 18:02:43 +0100

    Initial Commit

---
+++ TabBarSheet/TabBarSheetApp.swift
@@ -0,0 +1,14 @@
+//
+//  Created with ♥ by Serhii Pryimachuk on 10.11.2023.
+//  
+
+import SwiftUI
+
+@main
+struct TabBarSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
